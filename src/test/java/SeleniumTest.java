d0727a4903f6f92921ef6a9d81d982844b9e4927
Tanisi90 <69205416+Tanisi90@users.noreply.github.com>
2024-08-02 06:41:45 -0700

    Update SeleniumTest.java

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.PageLoadStrategy;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
